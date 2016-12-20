9409ca37ae6cfda8c47470878d23fba3d5fe672b
Dongdong <XUDONGDONG121@pingan.com.cn>
2016-12-20 10:57:34 +0800

    add 3DTouch

--- SnapKitDemo1/AppDelegate.swift
+++ SnapKitDemo1/AppDelegate.swift
@@ -13,7 +13,8 @@ import HelloWorld
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var launchedShortcutItem: UIApplicationShortcutItem?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -22,7 +23,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = BaseNavigationViewController(rootViewController: ViewController())
         self.window?.makeKeyAndVisible()
         Hello.sayHello()
-        return true
+        
+        var shouldPerformAdditionalDelegateHandling = true
+        
+        //需要检测一下传入的信息，如果能够找到 UIApplicationShortcutItem 对象，就进行针对性的处理
+        
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            
+            launchedShortcutItem = shortcutItem
+            // This will block "performActionForShortcutItem:completionHandler" from being called.
+            shouldPerformAdditionalDelegateHandling = false
+        }
+        
+        return shouldPerformAdditionalDelegateHandling
+    }
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        let hanledShortcutAction = handleShortCutItem(launchedShortcutItem!)
+        completionHandler(hanledShortcutAction)
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        guard let shortcut = launchedShortcutItem else { return }
+        
+        handleShortCutItem(shortcut)
+        
+        launchedShortcutItem = nil
+    }
+    
+    @discardableResult
+    func handleShortCutItem(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var hanle = false
+        if shortcutItem.type == "first" {
+           hanle = true
+        }
+        return hanle
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -37,10 +73,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
